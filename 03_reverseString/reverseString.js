db132c138c9315acd05484208620be45d1a8f4eb
ercsssn <ercsssn@gmail.com>
2022-03-03 19:36:03 +0800

    Solve JavaScript reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = Array.from(string);
+    let rev = [];
+    let lastChar = arr.length - 1;
+    
+    for (let i = lastChar ; i >= 0; i--) {
+        rev.push(arr[i]);
+    }
+    rev = rev.join('');
+    return rev;
 };
 
 // Do not edit below this line
