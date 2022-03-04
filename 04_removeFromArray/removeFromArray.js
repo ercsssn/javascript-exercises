af22637870da00e296059922cfd3f6a765b0fd1f
ercsssn <ercsssn@gmail.com>
2022-03-04 20:37:27 +0800

    Solve JavaScript removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,39 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    return array.filter(value => !args.includes(value));
+    
 };
+
+
+
+
+
+
+
+//array.forEach( (value) => {
+  //  if (!args.includes(value)){
+    //    newArray.push(value);
+    //}
+//});
+//return newArray;
+
+
+
+
+
+
+
+
+
+    //let remove = Array.from(elements);
+    //for (let i = 0; i < array.length; i++) {
+        //for (let j = 0; j < remove.length; i++){
+           // if (array[i] === remove[j]) {
+                //array.pop(i);
+            //}
+        //}
+    //}
+    //return array;
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
