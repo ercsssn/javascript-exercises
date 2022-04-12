7a9d29451c951efd500daaf2dcafd90c9b4cccac
ercsssn <ercsssn@gmail.com>
2022-04-12 16:53:55 +0800

    Solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    if (number <= 0) {
+        return 'OOPS';
+    }
+
+    nthTerm = (1/Math.sqrt(5)) * ((((1 + Math.sqrt(5))/2 )**number) - ((1 - Math.sqrt(5))/2 )**number);
+
+    return parseInt(nthTerm);
 };
 
 // Do not edit below this line
