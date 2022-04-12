789e71f026114f5434b33aa39d8d3985278a1927
ercsssn <ercsssn@gmail.com>
2022-04-12 15:51:47 +0800

    Solve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    let raw = words;
+    
+    punctuationless = raw.replace(/[.,!" "]/g,"");
+    finalWord = punctuationless.toLowerCase();
+    reversed = finalWord.split('').reverse().join('');
+    if (finalWord == reversed) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
-
 // Do not edit below this line
 module.exports = palindromes;
