02bf638027b4a653a2d6c48e7afc42525de1fecb
ercsssn <ercsssn@gmail.com>
2022-04-29 15:50:42 +0800

    Solve Javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const today = new Date();
+    for (i = 0; i <= arr.length-1; i++) {
+        if (!('yearOfDeath' in arr[i])) {
+            arr[i].yearOfDeath = today.getFullYear();
+        }
+    }
+    
+    const oldest = arr.sort(function(a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+      });
+    
+    return oldest[0];
 
 };
 
